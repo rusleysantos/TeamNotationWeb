f9e6baf6e2e3e39c165bfe0b609843a42e2df8ec
Rusley Santos <rusley.santos@uniodigital.com.br>
2021-03-31 00:38:23 -0300

    Altera proxy

--- proxy.config.js
+++ proxy.config.js
@@ -1,7 +1,7 @@
 const proxy = [
 	{
 		context: ['/api'],
-		target: 'http://localhost:57743',
+		target: 'https://apiteamnotation.azurewebsites.net',
 		secure: false,
 		logLevel: 'debug',
 		//pathRewrite: {'^/api' : ''}
